eb130e56f637f26cfbfea839f8e1c2f132812200
j1sebasM9 <jsebastian.mendoza@udea.edu.co>
2026-06-06 20:22:17 -0500

    Adaptacion clase obstaculo
    Se adapto la clase obstaculo para funcionar con el nivel1 y nivel2 en el mismo proyecto.

--- Control de versiones (commits)/obstaculo_v.h
+++ Control de versiones (commits)/obstaculo_v.h
@@ -1,7 +1,6 @@
 #ifndef OBSTACULO_H
 #define OBSTACULO_H
 
-
 #include <QGraphicsItem>
 #include <QPixmap>
 #include "Vector2D.h"
@@ -25,39 +24,34 @@ private:
     float radio;
     int daño;
     bool activo;
-    bool modoSumo;
 
+    float friccionModificada;
     float cronometroRayo;
     Vector2D zonaObjetivo;
     bool advertenciaActiva;
     float tiempoAviso;
     Vector2D velocidadMovimiento;
 
-    // Sprites
     QPixmap spriteActivo;
     QPixmap spriteAdvertencia;
 
-    // Método encapsulado para limpieza del constructor
     void cargarSprites();
 
 public:
-    Obstaculo(TipoObstaculo t, Vector2D pos, float r, float _tiempoAviso = 0.0f, bool _modoSumo = false);
+    Obstaculo(TipoObstaculo t, Vector2D pos, float r, float tiempoAviso = 0.0f);
 
-    // Métodos de Qt
     QRectF boundingRect() const override;
     void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget) override;
 
     void setVelocidadMovimiento(const Vector2D& v) { velocidadMovimiento = v; }
 
-    // Físicas y lógica
     void actualizarObstaculo(float dt);
     void aplicarEfecto(Combatiente* afectado, Combatiente* rival);
 
-    // Getters
     bool isActivo() const;
     TipoObstaculo getTipo() const;
     float getRadio() const;
     Vector2D getPosicion() const;
 };
 
-#endif // OBSTACULO_H
+#endif
